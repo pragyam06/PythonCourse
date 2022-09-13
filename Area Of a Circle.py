3e9deaaeab9337a0bce76682a3bfd61dd1e6d3c8
pragyam06 <111057800+pragyam06@users.noreply.github.com>
2022-09-13 18:01:49 +0530

    Created Area Of a Circle.py

---
+++ Area Of a Circle.py
@@ -0,0 +1,10 @@
+Task 1:
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+Task 2:
+fn=input("Enter Filename: ")
+f = fn.split(".")
+print ("Extension of the file is : " + f[-1])
